4489b34ab4dedabfadaac041e09f28518e423b84
Josh Lewis <44381254+joshlewis-py@users.noreply.github.com>
2019-05-21 21:24:23 +0100

    Uplaod of base file.
    This probably will not be added to again unless needed for future assignments or work...

---
+++ availability-calculator.py
@@ -0,0 +1,47 @@
+# welcome + menu select
+print(" Welcome to the Estimated System Avaliability Calculator ")
+
+def menu():
+  x=int(input("""\nTo find the estimated system availability, enter 1.
+To find the availability of a multiple component system, enter 2.\n"""))
+
+  if x==1:
+    estimatedAvailability()
+  if x==2:
+    multiComponentAvailability()
+  else:
+    print("invalid")
+    
+
+# If you know the mttr and mtbf this calcs the estimated availability 
+def estimatedAvailability():
+  print("\nRemeber to use seconds as an input")
+  mtbf = float(input("Enter MTBF: "))
+  print(mtbf)
+  mttr = float(input("Enter MTTR): "))
+  print (mttr)
+  total = mtbf/(mtbf+mttr)*100
+  print(total)
+
+
+# if you know the availaity of a component in % use this to work out availability of multiple components
+def multiComponentAvailability():
+  compList = []
+  total=1
+  i=0
+  n = int(input("Number of system components: "))
+  
+  for _ in range(n):
+    i=i+1
+    compListItem = float(input("Enter availability of component %s: "%i))
+    compList.append(compListItem/100)
+
+  for x in compList:
+    total *= x
+
+  print (total*100)
+
+
+#start
+menu()
+
